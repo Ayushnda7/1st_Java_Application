2efab021a3468fc8cddac23e653d2f9ae7811fe1
Ayush <ayush.nda7@gmail.com>
2021-05-26 23:38:00 +0530

    GUI Commit2

--- src/com/theglobaltalks/Main.java
+++ src/com/theglobaltalks/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("My Application");
-        JLabel label = new JLabel("Welcome to Java GUI",JLabel.CENTER);
+        JLabel label = new JLabel("Hello World !",JLabel.CENTER);
         frame.add(label);
         frame.setSize(400,400);
         frame.setVisible(true);
