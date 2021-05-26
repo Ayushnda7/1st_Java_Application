2a0e4cdeb28ab1145705c0cf36a83454260dce26
Ayush <ayush.nda7@gmail.com>
2021-05-26 23:35:19 +0530

    GUI Commit

--- src/com/theglobaltalks/Main.java
+++ src/com/theglobaltalks/Main.java
@@ -1,8 +1,14 @@
 package com.theglobaltalks;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        JFrame frame = new JFrame("My Application");
+        JLabel label = new JLabel("Welcome to Java GUI",JLabel.CENTER);
+        frame.add(label);
+        frame.setSize(400,400);
+        frame.setVisible(true);
     }
 }
